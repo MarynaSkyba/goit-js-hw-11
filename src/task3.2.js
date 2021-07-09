2d0ffa41b132b45c99851145c80a99c940d9b4f5
Maryna <marynaskyba@gmail.com>
2021-07-09 14:11:37 +0300

    add task 3.2

--- src/task3.2.js
+++ src/task3.2.js
@@ -3,19 +3,17 @@ const users = [
     { name: 'Poly', active: false },
     { name: 'Ajax', active: false },
   ];
-  
-  const toggleUserState = (allUsers, username, callback) => {
+
+
+  const toggleUserState = (allUsers, username) => {
+      return new Promise (resolve => {
     const updatedUsers = allUsers.map(user =>
       user.name === username ? { ...user, active: !user.active } : user
-    );
+    )
+    resolve (updatedUsers);
+  })
+};
   
-    callback(updatedUsers);
-  };
-  
-//   // Currently the function works like this
-//   toggleUserState(users, 'Mango', console.table);
-//   toggleUserState(users, 'Ajax', console.table);
-  
-//   // The function should work like this
-//   toggleUserState(users, 'Mango').then(console.table);
-//   toggleUserState(users, 'Ajax').then(console.table);
+  // The function should work like this
+  toggleUserState(users, 'Mango').then(console.table);
+  toggleUserState(users, 'Ajax').then(console.table);
