e38ec443558de689aa1a9f0cd24ab740cc511149
Maryna <marynaskyba@gmail.com>
2021-07-10 14:41:22 +0300

    timer

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './scss/main.css';
-// import './task1';
+import './task1';
 import './task2';
-// import './task3.1';
-// import './task3.2';
-// import './task3.3';
+import './task3.1';
+import './task3.2';
+import './task3.3';
 
 
--- src/task2.js
+++ src/task2.js
@@ -1,73 +1,87 @@
 
 const refs = {
 btnCount: document.querySelector('[data-startcount]'),
-clock: document.querySelector('.clockface'),
+clock: document.querySelector('.js-clockface'),
+input: document.getElementById('[date-selector]'),
+days: document.querySelector('[data-days]'),
+hours: document.querySelector('[data-hours]'),
+minutes: document.querySelector('[data-minutes]'),
+hours: document.querySelector('[data-seconds]'),
 };
 
-refs.btnCount.addEventListener('click', () => {start();});
 
-
-const countDownDate = new Date().getTime();
-console.log(countDownDate)
-
-
-let isActive = false;
-function start() {
-    if (isActive) {
-    return;
+class CountdownTimer {
+    constructor({targetDate}) {
+        this.targetDate = targetDate;
+        this.intervalId = null;
+        this.isActive = false;
+        this.refs = {
+            btnCount: document.querySelector('[data-startcount]'),
+            clock: document.querySelector('.js-clockface'),
+            input: document.getElementById('[date-selector]'),
+            days: document.querySelector('[data-days]'),
+            hours: document.querySelector('[data-hours]'),
+            minutes: document.querySelector('[data-minutes]'),
+            seconds: document.querySelector('[data-seconds]'),
+            };
     }
-    const startTime = Date.now();
-    isActive = true;
-    setInterval(() => {
-        const currentTime = Date.now();
-        const deltaTime = currentTime - startTime;
-        const timer= convertMs(deltaTime);
-        updateClockface(timer);
-    },1000);
-};
+start(){
+        if (this.isActive) {
+            return;
+        }
+   
+    this.isActive = true;    
+        setInterval(() => { 
+        const startTime = new Date();
+        const countTime = this.targetDate - startTime;
+        const timeCount= this.convertMs(countTime);
+        this.updateClockface(timeCount);
+        }
+    , 1000);
+    }
 
-
-//конечное время - текущее
-
-function updateClockface ({ days, hours, minutes, seconds }) {
-    refs.clock.textContent = `${days}: ${hours}: ${minutes}: ${seconds}`;
+convertMs(ms) {
+        const second = 1000;
+        const minute = second * 60;
+        const hour = minute * 60;
+        const day = hour * 24;
+      
+       
+        const days = this.pad(Math.floor(ms / day));
+        const hours = this.paddays(Math.floor((ms % day) / hour));
+        const minutes = this.paddays(Math.floor(((ms % day) % hour) / minute));
+        const seconds = this.paddays(Math.floor((((ms % day) % hour) % minute) / second));
+      
+        return { days, hours, minutes, seconds };
+      }
+      
+pad(value) {
+        return String(value).padStart(3,'0')};
+    
+paddays(value) {
+            return String(value).padStart(2,'0')};
+            
+updateClockface ({ days, hours, minutes, seconds }) {
+        this.refs.days.textContent = `${days}`;
+        this.refs.hours.textContent = `${hours}`;
+        this.refs.minutes.textContent = `${minutes}`;
+        this.refs.seconds.textContent = `${seconds}`;
+            }     
 }
 
-console.log(updateClockface);
 
+function updateClockface ({ days, hours, minutes, seconds }) {
+    this.refs.days.textContent = `${days}`;
+    this.refs.hours.textContent = `${hours}`;
+    this.refs.minutes.textContent = `${minutes}`;
+    this.refs.seconds.textContent = `${seconds}`;
+}
 
+const timer = new CountdownTimer({
+    targetDate: new Date('Jul 27, 2021'),
+});
 
+refs.btnCount.addEventListener('click', () => {
+    timer.start();
+});
 
-
-function pad(value) {
-    return String(value).padStart(3,'0')};
-
-function paddays(value) {
-        return String(value).padStart(2,'0')};
-
-
-
-
-
-function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = pad(Math.floor(ms / day));
-    // Remaining hours
-    const hours = paddays(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = paddays(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = paddays(Math.floor((((ms % day) % hour) % minute) / second));
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); //{days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6, minutes: 42, seconds: 20}
